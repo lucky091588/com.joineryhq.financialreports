5fcaa2bde5f58116ffa01b0f0992e702c0b38b5e
lucky091588 <lucky091588@gmail.com>
2020-07-25 00:56:05 +0800

    Confirm civicrm coding standards

--- financialreports.php
+++ financialreports.php
@@ -128,24 +128,24 @@ function financialreports_civicrm_alterSettingsFolders(&$metaDataFolders = NULL)
  * Implements hook_civicrm_preProcess().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
- *
-function financialreports_civicrm_preProcess($formName, &$form) {
+ */
+// function financialreports_civicrm_preProcess($formName, &$form) {
 
-} // */
+// } // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_navigationMenu
- *
-function financialreports_civicrm_navigationMenu(&$menu) {
-  _financialreports_civix_insert_navigation_menu($menu, NULL, array(
-    'label' => ts('The Page', array('domain' => 'com.joineryhq.financialreports')),
-    'name' => 'the_page',
-    'url' => 'civicrm/the-page',
-    'permission' => 'access CiviReport,access CiviContribute',
-    'operator' => 'OR',
-    'separator' => 0,
-  ));
-  _financialreports_civix_navigationMenu($menu);
-} // */
+ */
+// function financialreports_civicrm_navigationMenu(&$menu) {
+//   _financialreports_civix_insert_navigation_menu($menu, NULL, array(
+//     'label' => ts('The Page', array('domain' => 'com.joineryhq.financialreports')),
+//     'name' => 'the_page',
+//     'url' => 'civicrm/the-page',
+//     'permission' => 'access CiviReport,access CiviContribute',
+//     'operator' => 'OR',
+//     'separator' => 0,
+//   ));
+//   _financialreports_civix_navigationMenu($menu);
+// } // */
